dd195ce8dfb5c97e9da6d0c5b6cc5f3ffc5fa9e5
Evan Mitchell <30278906+evan-mitchell93@users.noreply.github.com>
2026-09-08 12:44:12 -0400

    Converting to Game Review CRUD

--- app.py
+++ app.py
@@ -1,74 +1,110 @@
 #Evan Mitchell
 #Created On: 10/7/2024
+#Updated On: 9/8/2026
 #Basic Crud operations to showcase knowledge of Flask and HTML
 #Utilizes a SQLite db
 #In the interest of saving time, the delete method is called via a get and does not utilize an alert to confirm 
 #the user intends to complete the action.
 
+####
+#Refactoring to be a game review/rating/played tracking system.
+####
+
 
 from flask import Flask, render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///crud.db'
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 #Athlete Model
-class Athlete(db.Model):
-    __tablename__ = 'Athletes'
+class Game(db.Model):
+    __tablename__ = 'Games'
     id = db.Column(db.Integer, primary_key=True, autoincrement=True )
-    firstname = db.Column(db.String(20))
-    lastname = db.Column(db.String(20))
-    jersey = db.Column(db.Integer)
-    positions = db.Column(db.String(20))
+    title = db.Column(db.String(50))
+    developer = db.Column(db.String(50))
+    publisher = db.Column(db.String(50))
+    available_platforms = db.Column(db.String(100))
 
-with app.app_context():
-    db.create_all()
+class Review(db.Model):
+    __tablename__ = 'Reviews'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    game_id = db.Column(db.Integer, db.ForeignKey('Games.id'))
+    review = db.Column(db.String(500))
+    rating = db.Column(db.Integer)
+    completed = db.Column(db.Boolean)
+    platform = db.Column(db.String(50))
+    user_id = db.Column(db.Integer)
 
-#Included in the index route is the ability to add new athletes
+#Included in the index route is the ability to add new games
 #this was done to showcase using a modal on the same page.
 #Edit and delete will use normal templating
 @app.route('/',methods = ['GET','POST'])
 def index():
     if request.method == "POST":
         #retrieve form data
-        firstname = request.form["firstname"]
-        lastname = request.form["lastname"]
-        jersey = request.form["jersey"]
-        positions = request.form["positions"]
+        title = request.form["title"]
+        developer = request.form["developer"]
+        publisher = request.form["publisher"]
+        available_platforms = request.form["available_platforms"]
         #create new instance of model
-        new_athlete = Athlete(firstname=firstname,lastname=lastname,jersey=jersey,positions=positions)
+        new_game = Game(title=title, developer=developer, publisher=publisher, available_platforms=available_platforms)
 
-        #add athlete to the db
-        db.session.add(new_athlete)
+        #add game to the db
+        db.session.add(new_game)
         db.session.commit()
         return redirect("/")
     elif request.method == "GET":
-        #show list of athletes
-        athletes = Athlete.query.all()
-        return render_template("index.html", athletes=athletes)
+        #show list of games
+        games = Game.query.all()
+        return render_template("index.html", games=games)
     
-#UPDATING ATHLETE INFO
+#UPDATING GAME INFO
 @app.route('/edit/<int:id>', methods=["GET","POST"])
-def edit_athlete(id):
-    athlete = Athlete.query.get(id)
+def edit_game(id):
+    game = Game.query.get(id)
     if request.method == "POST":
-        athlete.firstname = request.form["firstname"]
-        athlete.lastname = request.form["lastname"]
-        athlete.jersey = request.form["jersey"]
-        athlete.positions = request.form["positions"]
+        game.title = request.form["title"]
+        game.developer = request.form["developer"]
+        game.publisher = request.form["publisher"]
+        game.available_platforms = request.form["available_platforms"]
         db.session.commit()
         return redirect("/")
     else:
-        return render_template("edit.html", athlete=athlete)
+        return render_template("edit.html", game=game)
 
-#DELETING AN ATHLETE
-@app.route('/delete/<int:id>', methods=["GET"])
-def delete_athlete(id):
-    athlete = Athlete.query.get(id)
-    db.session.delete(athlete)
+#DELETING A GAME
+@app.route('/delete/<int:id>', methods=["POST"])
+def delete_game(id):
+    game = Game.query.get(id)
+    db.session.delete(game)
     db.session.commit()
     return redirect("/")
-    
+
+@app.route('/reviews')
+def reviews():
+    user_id = 1  # Replace with the actual user ID as needed
+    all_reviews = Review.query.filter_by(user_id=user_id).all()
+    return render_template("reviews.html", reviews=all_reviews)
+
+@app.route('/add_review', methods=["GET", "POST"])
+def add_review():
+    if request.method == "POST":
+        game_id = request.form["game_id"]
+        review = request.form["review"]
+        rating = request.form["rating"]
+        completed = request.form.get("completed") == "on"
+        platform = request.form["platform"]
+        user_id = 1  # Replace with the actual user ID as needed
+
+        new_review = Review(game_id=game_id, review=review, rating=rating, completed=completed, platform=platform, user_id=user_id)
+        db.session.add(new_review)
+        db.session.commit()
+        return redirect("/reviews")
+    games = Game.query.all()
+    return render_template("add_review.html", games=games)
 
 if __name__ == "__main__":
     app.run(debug=True)
